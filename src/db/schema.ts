25e8a8fde43767de8f46317a9c8b0570af3750d0
Jonatha Willian P Santos <jonathasantos.dev@hptmail.com>
2026-03-24 15:23:05 -0300

    Add patsTable

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,3 +7,9 @@ export const usersTable = pgTable("users", {
     email: varchar({length: 255}).notNull().unique(),
     obs: varchar({length: 255})
 });
+
+export const petsTable = pgTable("pets", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({length: 100}).notNull(),
+    ownerId: integer().notNull().references(() => usersTable.id)
+});
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { usersTable } from "../db/schema";
 import { db } from "../libs/drizzle";
-import { eq, sql } from "drizzle-orm";
+import { desc, eq, sql } from "drizzle-orm";
 
 const router = Router();
-
+//ping route
 router.get("/ping", (req, res) => {
     res.json({ pong: true });
 });
-
+//create user
 router.post("/user",  async (req, res) => {
     const newUser: typeof usersTable.$inferInsert = {
         name: "Teste 3 da Silva",
@@ -20,7 +20,7 @@ router.post("/user",  async (req, res) => {
     await db.insert(usersTable).values(newUser);
     res.status(201).json({ message: "Usuário criado com sucesso" });
 });
-
+//update user
 router.put("/user", async (req, res) => {
       await db 
         .update(usersTable)
@@ -31,7 +31,7 @@ router.put("/user", async (req, res) => {
 
     res.status(201).json({ message: "Usuário atualizado com sucesso" });
 });
-
+//delete user 
 router.delete("/user", async (req, res) => {
     await db
         .delete(usersTable)
@@ -39,8 +39,11 @@ router.delete("/user", async (req, res) => {
 
     res.status(201).json({ message: "Usuário deletado com sucesso" });
 });
-
+//get users
 router.get("/users", async (req, res) => {
+    let perPage = 2;
+    let currentPage = 1;
+
     const users = await db
     .select({
         name: sql<string>`lower(${usersTable.name})`,
@@ -48,7 +51,10 @@ router.get("/users", async (req, res) => {
         email: usersTable.email,
         codigo: usersTable.id
         })
-    .from(usersTable);
+    .from(usersTable)
+    .orderBy(desc(usersTable.name), desc(usersTable.id))
+    .limit(2)
+    .offset(1);
 
     res.json(users);
 });
